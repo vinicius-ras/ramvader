4f1d520e311f73a65799be4247da46dc0588cfdb
vinicius.ras <vinicius.ras@c4f62d13-1d80-41e3-aef9-1809c6e54439>
2013-07-27 22:08:29 +0000

    * RAMvaderGUI project: ** AddressEntry: *** Implemented the INotifyPropertyChanged interface for this class. Now, a PropertyChanged event is fired whenever the value of any of the properties of an AddressEntry instance get modified. *** Created private fields to back the properties that exist on the AddressEntry class. The properties are not "Auto-implemented properties" anymore: they use their corresponding new private fields explicitly. *** Removed the class' constructor method. The default value for each field is given on the field's declaration. ** MainWindow: *** The "refreshMemoryTable()" has been renamed to "executeMemoryOperations()". This method should execute every reading and writing operations on the target process' memory. Its documentation has also been updated. *** The static method "onMemoryTimerElapsed()" has been moved to the "static private methods" region. It was misplaced into the "non-static private methods" region.

--- trunk/RAMvaderGUI/AddressEntry.cs
+++ trunk/RAMvaderGUI/AddressEntry.cs
@@ -1,21 +1,23 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace RAMvaderGUI
 {
     /** Represents an address entry on the application's memory data grid. */
-    public class AddressEntry
+    public class AddressEntry : INotifyPropertyChanged
     {
-        #region PUBLIC PROPERTIES
+        #region PRIVATE FIELDS
         /** A user-defined description for the entry. */
-        public string Description { get; set; }
+        public string m_description = string.Empty;
         /** The address, on the target process, associated to this entry. */
-        public IntPtr Address { get; set; }
+        public IntPtr m_address = IntPtr.Zero;
         /** The type represented by this entry. */
-        public Type ValueType { get; set; }
+        public Type m_valueType = typeof( Int32 );
         /** A flag indicating if the value should be frozen or not. */
-        public bool Freeze { get; set; }
+        public bool m_freeze = false;
         /** The value associated to this entry. */
-        public Object Value { get; set; }
+        public Object m_value = new Int32();
         #endregion
 
 
@@ -25,15 +27,66 @@ namespace RAMvaderGUI
 
 
 
-        #region PUBLIC METHODS
-        /** Constructor. */
-        public AddressEntry()
+        #region PUBLIC PROPERTIES
+        /** A user-defined description for the entry. */
+        public string Description
         {
-            Description = string.Empty;
-            Address = IntPtr.Zero;
-            ValueType = typeof( Int32 );
-            Freeze = false;
-            Value = new Int32();
+            get { return m_description; }
+            set { m_description = value; onPropertyChanged(); }
+        }
+        /** The address, on the target process, associated to this entry. */
+        public IntPtr Address
+        {
+            get { return m_address; }
+            set { m_address = value; onPropertyChanged(); }
+        }
+        /** The type represented by this entry. */
+        public Type ValueType
+        {
+            get { return m_valueType; }
+            set { m_valueType = value; onPropertyChanged(); }
+        }
+        /** A flag indicating if the value should be frozen or not. */
+        public bool Freeze
+        {
+            get { return m_freeze; }
+            set { m_freeze = value; onPropertyChanged(); }
+        }
+        /** The value associated to this entry. */
+        public Object Value
+        {
+            get { return m_value; }
+            set { m_value = value; onPropertyChanged(); }
+        }
+        #endregion
+
+
+
+
+
+
+
+
+        #region EVENTS
+        /** Allows registering of PropertyChanged events. */
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+
+
+
+
+
+
+
+        #region PRIVATE METHODS
+        /** Called from the setter methods of the class' properties to notify when one
+         * of these properties have changed.
+         * @param propertyName The name of the property that has been changed. Automatically
+         *    filled with the name, because of its CallerMemberName attribute. */
+        private void onPropertyChanged( [CallerMemberName] string propertyName = "" )
+        {
+            if ( PropertyChanged != null )
+                PropertyChanged( this, new PropertyChangedEventArgs( propertyName ) );
         }
         #endregion
     }
