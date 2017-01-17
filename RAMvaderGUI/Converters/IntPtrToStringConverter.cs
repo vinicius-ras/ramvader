50969db86008647243bbf0de15352e6a93ee3da9
Vinicius Rogério Araujo Silva <31182306+vinicius-ras@users.noreply.github.com>
2017-01-17 19:06:18 +0000

    * RAMvaderTarget: created a new ReadFromTarget() method which is non-generic and accepts a Type argument, so that the client code can use it to read dynamic objects (by using their runtime types rather than compile-time types) from the target process' memory space. * RAMvaderGUI project has been rewritten from scratch, because its source code was obsolete and outdated. The new code is more maintainable and the features it provides are almost entirely based on WPF. The new RAMvaderGUI project currently provides no support for reading/writing IntPtr data from the target process - this feature is planned to be implemented in the future.

---
+++ RAMvaderGUI/Converters/IntPtrToStringConverter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace RAMvaderGUI
+{
+	/// <summary>
+	///    Converts <see cref="IntPtr"/> objects to string representations.
+	/// </summary>
+	[ValueConversion( typeof( IntPtr ), typeof( string ) )]
+	public class IntPtrToStringConverter : IValueConverter
+	{
+		#region INTERFACE IMPLEMENTATION: IValueConverter
+		public object Convert( object value, Type targetType, object parameter, CultureInfo culture )
+		{
+			if ( value == null || value.GetType() != typeof(IntPtr) )
+				return Binding.DoNothing;
+
+			IntPtr intPtrVal = (IntPtr) value;
+			return string.Format( "0x{0}", intPtrVal.ToString("X8") );
+		}
+
+
+		public object ConvertBack( object value, Type targetType, object parameter, CultureInfo culture )
+		{
+			string strVal = (string) value;
+			if ( strVal.StartsWith( "0x" ) )
+				strVal = strVal.Substring( 2 );
+
+			try {
+				int intVal = System.Convert.ToInt32( strVal, 16 );
+				return new IntPtr( intVal );
+			}
+			catch ( FormatException )
+			{
+				return Binding.DoNothing;
+			}
+			catch (Exception)
+			{
+				throw;
+			}
+		}
+		#endregion
+	}
+}
---
+++ RAMvaderGUI/Windows/AddRAMvaderTestTargetAddressesDialog.xaml.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace RAMvaderGUI
+{
+	/// <summary>
+	///    Interaction logic for AddRAMvaderTestTargetAddressesDialog.xaml.
+	/// </summary>
+	public partial class AddRAMvaderTestTargetAddressesDialog : Window
+	{
+		#region PRIVATE FIELDS
+		/// <summary>
+		///    Keeps the addresses of the variables of the RAMvaderTestTarget program, which can be querried by
+		///    their respective <see cref="Type"/>.
+		/// </summary>
+		private Dictionary<Type,IntPtr> m_variableAddresses = new Dictionary<Type, IntPtr>();
+		#endregion
+
+
+
+
+
+		#region PRIVATE METHODS
+		/// <summary>Parses the given string for its corresponding hex-value.</summary>
+		/// <param name="str">The string to be parsed.</param>
+		/// <returns>
+		///    In case of success, returns the hex-value corresponding to the given string.
+		///    In case of failure, returns <code>null</code>.
+		/// </returns>
+		private Int32? GetHexValueFromString( string str )
+		{
+			// Prepare the string for testing
+			str = str.Trim().ToLower();
+			if ( str.StartsWith( "0x" ) )
+				str = str.Substring( 2 );
+
+			if ( string.IsNullOrEmpty( str ) == false )
+			{
+				try
+				{
+					Int32 typedValue = Convert.ToInt32( str, 16 );
+					return typedValue;
+				}
+				catch ( ArgumentException ) { }
+				catch ( FormatException ) { }
+				catch ( OverflowException ) { }
+			}
+
+			return null;
+		}
+		#endregion
+
+
+
+
+
+		#region PUBLIC METHODS
+		/// <summary>Constructor.</summary>
+		public AddRAMvaderTestTargetAddressesDialog()
+		{
+			InitializeComponent();
+		}
+
+
+		/// <summary>Retrieves the address the user typed for a given variable of the RAMvaderTestTarget program.</summary>
+		/// <param name="targetType">The type of the variable from RAMvaderTestTarget program whose address is to be retrieved.</param>
+		/// <returns>Returns the address of the variable, input by the user.</returns>
+		public IntPtr RetrieveTypedAddress( Type targetType )
+		{
+			IntPtr result;
+			if ( m_variableAddresses.TryGetValue( targetType, out result ) == false )
+				result = IntPtr.Zero;
+			return result;
+		}
+		#endregion
+
+
+
+
+
+		#region EVENT CALLBACKS
+		/// <summary>Called when the user clicks the "OK" button of the dialog.</summary>
+		/// <param name="sender">The object which sent the event.</param>
+		/// <param name="e">Holds data about the event.</param>
+		private void ButtonOkClick( object sender, RoutedEventArgs e )
+		{
+			string typedText = m_txtAddresses.Text.Replace( "\r", string.Empty );
+
+			// Verify if there are the exact number of expected lines in the user's input
+			string [] linesToRead = typedText.Split( new string[] { "\n" }, StringSplitOptions.RemoveEmptyEntries );
+			int totalExpectedInputAddresses = RAMvaderTestTargetData.ExpectedAddressesInputTypeOrder.Length;
+			if ( linesToRead.Length != totalExpectedInputAddresses )
+			{
+				string errorMsg = string.Format( Properties.Resources.strErrorRAMvaderTestTargetNotEnoughLinesInInputMsg,
+					totalExpectedInputAddresses, linesToRead.Length );
+				MessageBox.Show( this, errorMsg, Properties.Resources.strErrorMalformedInput, MessageBoxButton.OK,
+					MessageBoxImage.Error );
+				return;
+			}
+
+			// Obtain the input addresses of the variables
+			m_variableAddresses.Clear();
+			for ( int lineIndex = 0; lineIndex < linesToRead.Length; lineIndex++ )
+			{
+				// Try to parse the hex value
+				Int32? hexValue = this.GetHexValueFromString( linesToRead[lineIndex] );
+				if ( hexValue.HasValue == false )
+				{
+					string errorMsg = string.Format( Properties.Resources.strErrorRAMvaderTestTargetInvalidLine,
+						lineIndex+1, linesToRead[lineIndex] );
+					MessageBox.Show( this, errorMsg, Properties.Resources.strErrorMalformedInput, MessageBoxButton.OK,
+						MessageBoxImage.Error );
+					return;
+				}
+
+				// Add to the result
+				Type curType = RAMvaderTestTargetData.ExpectedAddressesInputTypeOrder[lineIndex];
+				m_variableAddresses.Add( curType, new IntPtr( hexValue.Value ) );
+			}
+
+			// Everything ok: close the dialog, returning true
+			this.DialogResult = true;
+			this.Close();
+		}
+		#endregion
+	}
+}
